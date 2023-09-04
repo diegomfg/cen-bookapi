074c0bacb5e97fab533b877216fc25fbec606f39
Diego Matheus <diegoamatheus@gmail.com>
2023-09-04 15:50:10 -0400

    Add base route for testing

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapGet("/", () => "Base Route, testing minimal routes");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
