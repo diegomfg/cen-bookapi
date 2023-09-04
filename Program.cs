fc7dbb0f55d9e95946dcdaaf36f260ae8b34fc52
Diego Matheus <diegoamatheus@gmail.com>
2023-09-04 15:41:40 -0400

    testing commit 2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Testing commit
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
