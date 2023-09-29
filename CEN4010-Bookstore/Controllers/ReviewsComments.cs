cc831c39dcad4bbf8d81da022bb2274b253c0035
Emmanuel Mejia <132930638+EtherealCereal01@users.noreply.github.com>
2023-09-29 14:13:09 -0400

    (emmanuel mejia) added controller for book reviews and comments

---
+++ CEN4010-Bookstore/Controllers/ReviewsComments.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CEN4010_Bookstore.Models;
