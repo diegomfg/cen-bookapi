c9e1cde15831b19890f5b2ae633cc5650b6599af
Natanael06 <71079068+Natanael06@users.noreply.github.com>
2023-10-13 15:50:19 -0400

    ShoppingCart controller added
    Added a shopping cart controller to the Admin/controller section

---
+++ CEN4010-Bookstore/Areas/Admin/Controllers/ShoppingCartController.cs
@@ -0,0 +1,28 @@
+﻿using CEN4010_Bookstore.Data;
+using CEN4010_Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CEN4010_Bookstore.Areas.Admin.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public List<ShoppingCart> GetShoppingCartProfiles()
+        {
+            List<ShoppingCart> SCProfiles = _db.ShoppingCarts.ToList();
+            return SCProfiles;
+
+        }
+    }
+}
