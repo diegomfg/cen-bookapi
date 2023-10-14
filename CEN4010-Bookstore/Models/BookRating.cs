9bc7824247ac00bfb6d1a6856ca1afc2d68ce730
Diego Matheus <diegoamatheus@gmail.com>
2023-10-13 22:24:34 -0400

    Add primary key to BookRating and BookReview. Commented some code that stopped the project from running

--- CEN4010-Bookstore/Models/BookRating.cs
+++ CEN4010-Bookstore/Models/BookRating.cs
@@ -1,7 +1,11 @@
-﻿namespace CEN4010_Bookstore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CEN4010_Bookstore.Models
 {
     public class BookRating
     {
+        [Key]
+        public int Id { get; set; }
         public int userId { get; set; }
         public Book BookId { get; set; }
         public int Rating { get; set; }
